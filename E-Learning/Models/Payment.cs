e0adec4122a365cf5ea3b1fa66b404634fcd7411
AhmedHassan <hasnm8722@gmail.com>
2024-09-20 00:43:29 +0300

    Create Classes in Model Folder and Handel Relations Between Tables and add Migiration "CreateDatabase"

---
+++ E-Learning/Models/Payment.cs
@@ -0,0 +1,11 @@
+﻿namespace E_Learning.Models
+{
+    public class Payment
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string UserId { get; set; }
+        public int CourseId { get; set; }
+    }
+}
