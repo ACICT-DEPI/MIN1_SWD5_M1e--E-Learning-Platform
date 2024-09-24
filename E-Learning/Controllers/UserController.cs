e247f2cc8dadaead1a57836714d04aa494e7b713
AhmedHassan <hasnm8722@gmail.com>
2024-09-25 02:55:04 +0300

    change in user controler

--- E-Learning/Controllers/UserController.cs
+++ E-Learning/Controllers/UserController.cs
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-﻿using Microsoft.AspNetCore.Identity.Data;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Learning.Controllers
@@ -12,26 +10,4 @@ namespace E_Learning.Controllers
 			return View("Login");
 		}
 	}
-=======
-﻿using Microsoft.AspNetCore.Mvc;
-=======
-
-using Microsoft.AspNetCore.Mvc;
-
->>>>>>> 5e1e916d79906d70d704905bd54a1dc9563eb020
-
-namespace E_Learning.Controllers
-{
-    public class UserController : Controller
-    {
-        public IActionResult Index()
-        {
-            return View("Login");
-        }
-    }
-<<<<<<< HEAD
->>>>>>> Hassan
-=======
-
->>>>>>> 5e1e916d79906d70d704905bd54a1dc9563eb020
 }
