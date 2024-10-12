3cc47c1c3204cfd2708f45d9b0a9185db5de11e3
ahmed-dyab <HP@DESKTOP-3IAUT5P>
2024-10-12 23:27:19 +0300

    slove merge convlect

--- Repositories/Interfaces/INoteRepository.cs
+++ Repositories/Interfaces/INoteRepository.cs
@@ -10,13 +10,8 @@ namespace Repositories.Interfaces
 {
     public interface INoteRepository
     {
-<<<<<<< HEAD
         public Task<IQueryable<Note>> GetAllNotesByLessonId(int lessonId,bool istracked);
-        public Task<IQueryable<Note>> GetAllNotesByCourseId(int courseId,bool istracked);
-=======
-       public Task<IQueryable<Note>> GetAllNotesByLessonId(int lessonId,bool istracked);
-       public Task<Note> GetNoteById(int noteId,bool istracked);
->>>>>>> 8e10893e88dd6c86584d62f2f556832a856ee210
+        public Task<Note> GetNoteById(int noteId,bool istracked);
         public Task<ResponseVM> CreateNote (Note note);
         public Task<ResponseVM> UpdateNote (Note note);
         public Task<ResponseVM> DeleteNote (Note note);
