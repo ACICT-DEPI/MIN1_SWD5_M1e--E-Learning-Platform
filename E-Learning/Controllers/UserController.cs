b62b7534e3789c98ca27cb9b2aab057572e23263
Ragab20 <ragabhakim2020@gmail.com>
2024-10-12 22:43:23 +0300

    login services

--- E-Learning/Controllers/UserController.cs
+++ E-Learning/Controllers/UserController.cs
@@ -1,14 +1,24 @@
 
+using Enities.ViweModel.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Learning.Controllers
 {
     public class UserController : Controller
     {
+        [HttpGet]
         public IActionResult Login()
         {
             return View("Login");
         }
+        [HttpPost]
+		public IActionResult Login(LoginVM model)
+		{
+			if (ModelState.IsValid) { 
+			  
+			}
+			return View();
+		}
 		public IActionResult Register()
 		{
 			return View("Register");
---
+++ Services/Impelmentations/UserServices.cs
@@ -0,0 +1,35 @@
+﻿using AutoMapper;
+using Enities.ViweModel;
+using Enities.ViweModel.User;
+using Entites.Models;
+using Microsoft.AspNetCore.Identity;
+using Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Impelmentations
+{
+	public sealed class UserServices : IUserService
+	{
+		private readonly SignInManager<User> _signInManager;
+		private readonly IMapper _mapper;
+
+		public UserServices(SignInManager<User> signInManager,IMapper mapper) {
+			_signInManager = signInManager;
+			_mapper = mapper;
+		}
+		public async Task<ResponseVM> loginprocess(LoginVM model)
+		{
+			var user = _mapper.Map<User>(model);
+			throw new NotImplementedException();
+		}
+
+		public Task<ResponseVM> Registerprocess(LoginVM model)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
