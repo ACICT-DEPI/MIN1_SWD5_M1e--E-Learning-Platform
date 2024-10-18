cd1780cacb7b6253c86f6f8d275ef34c05da0d75
AhmedHassan <hasnm8722@gmail.com>
2024-10-19 01:03:27 +0300

    change userid in courde services

--- Services/Interfaces/ICourseServices.cs
+++ Services/Interfaces/ICourseServices.cs
@@ -7,11 +7,12 @@ namespace Services.Interfaces
 {
     public interface ICourseServices
     {
-       Task<List<Course>> GetAllCourcesAsync(bool istraked);
-        Task<Course> GetCourseByIdAsync(int id, bool istracked);
-        Task<ResponseVM> CreateNewCourse(CreateorUpdateCourseVM course);
-        Task<ResponseVM> UpdateCourse(CreateorUpdateCourseVM courseVM,int id);
-        Task<ResponseVM> DeleteCourseAsync(int id);
+      public Task<List<Course>> GetAllCourcesAsync(bool istraked);
+      public  Task<Course> GetCourseByIdAsync(int id, bool istracked);
+       public Task<List<Course>> GetCourseByUserId(string userId, bool istracked);
+       public Task<ResponseVM> CreateNewCourse(CreateorUpdateCourseVM course);
+       public Task<ResponseVM> UpdateCourse(CreateorUpdateCourseVM courseVM,int id);
+       public Task<ResponseVM> DeleteCourseAsync(int id);
 
     }
 }
