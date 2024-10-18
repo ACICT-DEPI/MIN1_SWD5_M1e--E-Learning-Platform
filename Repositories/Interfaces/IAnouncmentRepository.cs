bf20a087698ddb26dd7a4f5b88a14f04144c4821
AbobakrMahdy <abwbkrmhdy0@gmail.com>
2024-10-18 23:52:03 +0300

    change layout

---
+++ Repositories/Interfaces/IAnouncmentRepository.cs
@@ -0,0 +1,20 @@
+﻿using Enities.ViweModel;
+using Entites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Interfaces
+{
+    public interface IAnouncmentRepository
+    {
+        public Task<IEnumerable<Anouncment>> GetAllAnouncmentsByCourseId(int courseId,bool istracked);
+        public Task<Anouncment> GetAnouncmentsById(int Id);
+        public Task<ResponseVM> CreateNewAnoundment(Anouncment anouncment);
+        public Task<ResponseVM> UpdateNewAnoundment(Anouncment anouncment);
+        public Task<ResponseVM> DeleteNewAnoundment(Anouncment anouncment);
+       
+    }
+}
